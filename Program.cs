cf61a827e1d58041b8ad87adcaa95bce996f49df
danielmontes9 <gdanielmontesh9@gmail.com>
2023-04-01 00:18:07 -0600

    config db in memory

--- Program.cs
+++ Program.cs
@@ -1,6 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using project_ef;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<TareasContext>(p => p.UseInMemoryDataBase("TareasDb"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/dbconexion", async ([FromServices] TareasContext dbContext) => 
+{
+	dbContext.DataBase.EnsureCreated();
+	return Results.Ok("Base de datos en memoria: " + dbContext.DataBase.IsInMemory());
+
+});
 
 app.Run();
