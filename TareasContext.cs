db952966c243111a4dcb78fd08b8a7d2034d9e24
danielmontes9 <gdanielmontesh9@gmail.com>
2023-03-31 23:47:49 -0600

    add context

---
+++ TareasContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using project_ef.Models;
+
+namespace project_ef;
+
+public class TareasContext: DbContext
+{
+
+	public DbSet<Categoria> Categorias {get;set;}
+
+	public DbSet<Tarea> Tareas {get;set;}
+
+	public TareasContext(DbContextOptions<TareasContext> options): base (options) { }
+
+}
